8635b339897d378d56259c1e2ddfa8384bb70dce
Verenich Tatiana <verenik91@gmail.com>
2024-03-26 00:07:18 +0300

    домашнее задание 3, work with dataBase

--- seminars/DataBase/src/main/java/org/example/homeWork/Main.java
+++ seminars/DataBase/src/main/java/org/example/homeWork/Main.java
@@ -4,23 +4,48 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            String url = "jdbc:mysql://127.0.0.1:3307/?user=tania/java";
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            try (Connection conn = DriverManager.getConnection(url)) {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.err.println("Не удалось найти класс драйвера.");
+//            e.printStackTrace();
+//        }
 
-                Statement statement = conn.createStatement();
-                int rows = statement.executeUpdate("""
-                        INSERT students(first_name, last_name, age) VALUES\s
-                                                ('Alla', 'Verenich', 34),
-                                                 ('Lari', 'Crevice', 29)
-                        """);
-                System.out.printf("Added %d rows", rows);
+        String url = "jdbc:mysql://localhost:3306/?user=root";
+        String user = "root";
+        String password = "329960q";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            acceptConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("Ошибка при создании соединения.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void acceptConnection(Connection connection) throws SQLException {
+        insertData(connection);
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM java.students");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                System.out.println("id = " + id + ", name = " + firstName + ", last_name = " + lastName + ", age = " + age);
             }
-        } catch (Exception ex) {
-            System.out.println("Connection failed...");
+        }
+    }
 
-            System.out.println(ex);
+    private static void insertData(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("""
+                    INSERT INTO java.students(first_name, last_name, age) VALUES
+                     ('Alla', 'Verenich', 34),
+                     ('Lari', 'Crevice', 29);
+                    """);
         }
     }
 }
